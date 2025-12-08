1c46ea2a01086eec3397c9f5435e4eec0dd720a0
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-09 02:42:24 +0330

    +

--- training_script/BERT/get_features_glue.py
+++ training_script/BERT/get_features_glue.py
@@ -475,12 +475,57 @@ def main():
     # print('train_dataset', len(train_dataset))
     # print('eval_dataset', len(eval_dataset))
     import torch.nn as nn
+    from transformers.trainer import unwrap_model, is_peft_available, MODEL_FOR_CAUSAL_LM_MAPPING_NAMES
+    from peft import PeftModel
+    
     class CustomTrainer(Trainer):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)   # <-- REQUIRED
             self.hamed_pooled_features = []
             self.hamed_pooled_labels = []
 
+        def compute_loss(self, model, inputs, return_outputs=False): #[?]
+            """
+            How the loss is computed by Trainer. By default, all models return the loss in the first element.
+
+            Subclass and override for custom behavior.
+            """
+            if self.label_smoother is not None and "labels" in inputs:
+                labels = inputs.pop("labels")
+            else:
+                labels = None
+            outputs = model(**inputs)
+
+            features_befor_clf = model.dropout(model.bert.pooler(outputs.hidden_states[-1])) 
+            self.hamed_pooled_features.append(features_befor_clf.cpu().detach().numpy().squeeze())
+            self.hamed_pooled_labels.append(inputs['labels'].cpu().detach().numpy().squeeze())
+
+            # Save past state if it exists
+            # TODO: this needs to be fixed and made cleaner later.
+            if self.args.past_index >= 0:
+                self._past = outputs[self.args.past_index]
+            
+            if labels is not None:
+                unwrapped_model = unwrap_model(model)
+                if is_peft_available() and isinstance(unwrapped_model, PeftModel):
+                    model_name = unwrapped_model.base_model.model._get_name()
+                else:
+                    model_name = unwrapped_model._get_name()
+                if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
+                    loss = self.label_smoother(outputs, labels, shift_labels=True)
+                else:
+                    loss = self.label_smoother(outputs, labels)
+            else:
+                if isinstance(outputs, dict) and "loss" not in outputs:
+                    raise ValueError(
+                        "The model did not return a loss from the inputs, only the following keys: "
+                        f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
+                    )
+                # We don't use .loss here since the model may return tuples instead of ModelOutput.
+                loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+            return (loss, outputs) if return_outputs else loss
+        
         def compute_loss(self, model: nn.Module, inputs, return_outputs: bool = False, num_items_in_batch = None):
             """
             How the loss is computed by Trainer. By default, all models return the loss in the first element.
@@ -516,10 +561,6 @@ def main():
                 inputs = {**inputs, **kwargs}
             outputs = model(**inputs)
             
-            features_befor_clf = model.dropout(model.bert.pooler(outputs.hidden_states[-1])) 
-            self.hamed_pooled_features.append(features_befor_clf.cpu().detach().numpy().squeeze())
-            self.hamed_pooled_labels.append(inputs['labels'].cpu().detach().numpy().squeeze())
-
             # User-defined compute_loss function
             if self.compute_loss_func is not None:
                 if labels is None:
