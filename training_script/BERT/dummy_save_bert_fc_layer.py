46418c97fa4803e29a35a82b8f669cf67d453d14
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-09 23:59:08 +0330

    +

--- training_script/BERT/dummy_save_bert_fc_layer.py
+++ training_script/BERT/dummy_save_bert_fc_layer.py
@@ -1,10 +1,27 @@
 import torch
 from safetensors.torch import load_file
+from transformers.models.bert import BertForSequenceClassification
+"""
+For bert-base-cased:
+.... 'bert.encoder.layer.9.intermediate.dense.bias', 'bert.encoder.layer.9.intermediate.dense.weight', 'bert.encoder.layer.9.output.LayerNorm.bias', 'bert.encoder.layer.9.output.LayerNorm.weight', 'bert.encoder.layer.9.output.dense.bias', 'bert.encoder.layer.9.output.dense.weight', 'bert.pooler.dense.bias', 'bert.pooler.dense.weight', 'classifier.bias', 'classifier.weight']
+"""
+
+# ******* the nn.Dropout does not have parameters ********** # Dropout(p=0.1, inplace=False)
 
 weights = load_file("/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/BERT/models/no_trainer/sst2/model.safetensors")
 print(weights.keys())
-print(type(weights['classifier']))
+print(type(weights['classifier.weight']))
 print(type(weights['dropout']))
+
+
+# import torch.nn as nn 
+# class BertCLF(nn.module): 
+#     def __init__(self, ): 
+#         self.dropout = nn.Dropout(0.1)
+#         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+#     pass
+
 
 """
 later do this: 
