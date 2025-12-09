dcec4d991fcbf3a7639ba80c6d7c374aeaca8e83
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-10 01:03:51 +0330

    +

--- training_script/BERT/scripts/test_sst2_advglue.sh
+++ training_script/BERT/scripts/test_sst2_advglue.sh
@@ -5,7 +5,7 @@ batch_size=32
 torchrun --nproc_per_node=${num_gpus} \
   run_glue_no_trainer.py \
   --validation_file 'dev.json' \
-  --model_name_or_path bert-base-cased \
+  --model_name_or_path '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/BERT/models/no_trainer/sst2' \
   --task_name $TASK_NAME \
   --max_length 128 \
   --pad_to_max_length \
