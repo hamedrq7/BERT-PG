634d9dac4fb11d93e2cdf898c542c07a92372ee9
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-10 16:15:13 +0330

    +

--- training_script/BERT/scripts/test_sst2_advglue.sh
+++ training_script/BERT/scripts/test_sst2_advglue.sh
@@ -2,6 +2,7 @@ TASK_NAME=sst2
 num_gpus=1
 batch_size=32
 
+# Adv 
 torchrun --nproc_per_node=${num_gpus} \
   run_glue_no_trainer.py \
   --task_name $TASK_NAME \
@@ -15,3 +16,15 @@ torchrun --nproc_per_node=${num_gpus} \
   --with_tracking \
   --output_dir ./models/no_trainer/$TASK_NAME/adv_glue/ \
   --eval_adv_glue
+
+torchrun --nproc_per_node=${num_gpus} \
+  run_glue_no_trainer.py \
+  --task_name $TASK_NAME \
+  --model_name_or_path '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/BERT/models/no_trainer/sst2' \
+  --max_length 128 \
+  --pad_to_max_length \
+  --per_device_eval_batch_size ${batch_size} \
+  --seed 42 \
+  --with_tracking \
+  --output_dir ./models/no_trainer/$TASK_NAME/adv_glue/ \
+  --eval_clean_glue
