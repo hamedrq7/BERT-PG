414beae5190991f6b49dc597c40d130850866125
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-10 16:01:43 +0330

    +

--- training_script/BERT/wSodef_run_glue_no_trainer.py
+++ training_script/BERT/wSodef_run_glue_no_trainer.py
@@ -306,6 +306,100 @@ def parse_args():
 
 
 import torch.nn as nn 
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+import geotorch
+
+class newLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(newLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(in_features,out_features))
+#         self.weight = self.weighttemp.T
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        return F.linear(input, self.weight.T, self.bias)
+
+    def extra_repr(self) -> str:
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
+class ORTHFC(nn.Module):
+    def __init__(self, dimin, dimout, bias):
+        super(ORTHFC, self).__init__()
+        if dimin >= dimout:
+            self.linear = newLinear(dimin, dimout,  bias=bias)
+        else:
+            self.linear = nn.Linear(dimin, dimout,  bias=bias)
+        geotorch.orthogonal(self.linear, "weight")
+
+    def forward(self, x):
+        return self.linear(x)
+    
+class MLP_OUT_ORTH_X_X(nn.Module):
+    def __init__(self, dim1, dim2):
+        super(MLP_OUT_ORTH_X_X, self).__init__()
+        self.fc0 = ORTHFC(dim1, dim2, False)
+    def forward(self, input_):
+        h1 = self.fc0(input_)
+        return h1
+
+class ODEBlock(nn.Module):
+
+    def __init__(self, odefunc):
+        super(ODEBlock, self).__init__()
+        self.odefunc = odefunc
+        self.integration_time = torch.tensor([0, 5]).float()
+
+    def forward(self, x):
+        self.integration_time = self.integration_time.type_as(x)
+        out = odeint(self.odefunc, x, self.integration_time, rtol=1e-3, atol=1e-3)
+        return out[1]
+
+    @property
+    def nfe(self):
+        return self.odefunc.nfe
+
+    @nfe.setter
+    def nfe(self, value):
+        self.odefunc.nfe = value    
+
+class ConcatFC(nn.Module):
+
+    def __init__(self, dim_in, dim_out):
+        super(ConcatFC, self).__init__()
+        self._layer = nn.Linear(dim_in, dim_out)
+    def forward(self, t, x):
+        return self._layer(x)
+
+class ODEfunc_mlp(nn.Module): 
+
+    def __init__(self, dim):
+        super(ODEfunc_mlp, self).__init__()
+        self.fc1 = ConcatFC(64, 64)
+        self.act1 = torch.sin 
+        self.nfe = 0
+
+    def forward(self, t, x):
+        self.nfe += 1
+        out = -1*self.fc1(t, x)
+        out = self.act1(out)
+        return out
+    
 class Phase3Model(nn.Module): 
     def __init__(self, bridge_768_64, ode_block, fc): 
         super(Phase3Model, self).__init__()
@@ -477,15 +571,24 @@ def main():
 
     # load sodef module and clf part
     import torch 
-    sodef = Phase3Model(None, None, None)
+    feature_dim = 768
+    bridge_dim = 64
+    sodef = Phase3Model(
+        bridge_768_64=MLP_OUT_ORTH_X_X(feature_dim, bridge_dim), 
+        ode_block=ODEBlock(odefunc=ODEfunc_mlp(0)), 
+        fc=nn.Linear(feature_dim, num_labels)
+        )
     saved_temp = torch.load(args.sodef_model)
     statedic_temp = saved_temp['phase3_model']
     sodef.load_state_dict(statedic_temp)
+    print('Sodef module: ')
+    print(sodef)
 
     model.dropout.p = 0.0
     model.classifier = sodef
+    print('Model after combinign w SODEF')
     print(model)
-    
+
 
     # Preprocessing the datasets
     if args.task_name is not None:
