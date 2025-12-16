24be8de78780f2b29ca16a4b4085b99bd964fbad
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-16 15:23:31 +0330

    +

--- training_script/DeBERTa/scripts/get_feats.sh
+++ training_script/DeBERTa/scripts/get_feats.sh
@@ -59,5 +59,5 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=${num_gpus} --master_port=29501
   --logging_dir $EXP_DIR/$EXP_INDEX \
   --save_total_limit 1 \
   --eval_accumulation_steps 1 \
-  --max_val_samples 5 \
+#   --max_val_samples 5 \
 #   --max_train_samples 100 \
