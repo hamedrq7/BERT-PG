1d71548c4e46eea3e609482896bc732d06b056cc
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-17 02:36:03 +0330

    +

--- training_script/DeBERTa/SODEF/sodef_runs.sh
+++ training_script/DeBERTa/SODEF/sodef_runs.sh
@@ -92,7 +92,7 @@ FIXED_ARGS="--train_feature_set_dir $TR_FEATURE_DIR \
 
 # (r1, r2, r3)
 reg_sets=(
-  "10.0 1.0 0.1", 
+  "10.0 1.0 0.1"
   "10.0 10.0 0.2"
 )
 
@@ -195,98 +195,6 @@ for reg_set in "${reg_sets[@]}"; do
                     echo "$CMD"
                     eval $CMD
 
-                done
-            done
-        done
-    done
-done
-
-
-# ----------- CONSTANT ARGUMENTS -----------
-
-TR_FEATURE_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/train_features.npz"
-TE_FEATURE_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/test_features.npz"
-PHASE1_MODEL="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaFirstSODEF/Phase1-Tuning/eps_-optim_SGD-lr_1e-3-eps_/phase1/phase1_best_acc_ckpt.pth"
-ADV_GULE_DOR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/advglue_features.npz"
-
-FIXED_ARGS="--train_feature_set_dir $TR_FEATURE_DIR \
-            --test_feature_set_dir $TE_FEATURE_DIR \
-            --phase1_model_path $PHASE1_MODEL \
-            --adv_glue_feature_set_dir $ADV_GULE_DOR \
-            --seed 100 \
-            --phase2_batch_size 64 \
-            --phase2_numm 64 \
-            --phase2_epoch 5 \
-            --bert_feature_dim 1024" 
-
-# ----------- EXPERIMENT VALUE LISTS -----------
-reg1_list=(10. )
-reg2_list=(1.0) # 0.01 0.1 0.0001 
-reg3_list=(0.1) #  0.01
-
-phase2_exponent=1.0
-phase2_exponent_off=0.1
-phase2_exponent_f=50.
-phase2_time_df=1.
-phase2_trans=1.
-phase2_trans_off_diag=1.
-phase2_integration_time=5.
-
-# Two binary toggles:
-decay_options=("on") #  "off"
-default_adam=("on") 
-no_prevs=("off")
-# ----------- LOOP OVER ALL COMBINATIONS -----------
-for r1 in "${reg1_list[@]}"; do
-    for r2 in "${reg2_list[@]}"; do
-        for r3 in "${reg3_list[@]}"; do
-            for decay in "${decay_options[@]}"; do
-                for optim in "${default_adam[@]}"; do
-                    for no_prev in "${no_prevs[@]}"; do
-
-                        # ---------------- Construct optional args ----------------
-                        decay_arg=""
-                        optim_args=""
-                        no_prev_args=""
-
-                        if [[ "$decay" == "on" ]]; then
-                            decay_arg="--decay_lr"
-                        fi
-
-                        if [[ "$optim" == "on" ]]; then
-                            optim_args="--phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
-                        fi
-
-                        if [[ "$optim" == "sgd" ]]; then
-                            optim_args=" --phase2_optim SGD --phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
-                        fi
-                        
-                        if [[ "$no_prev" == "on" ]]; then
-                            no_prev_args=" --no_phase2_use_fc_from_phase1 --no_phase3_use_fc_from_phase2"
-                        fi 
-
-                        # ---------------- Construct experiment name ----------------
-                        exp_name="r1=${r1}_r2=${r2}_r3=${r3}_default_adam=${optim}_no_prev${no_prev}"
-
-                        # ---------------- Output directory ----------------
-                        output_dir="../DeBERTaFirstSODEF-Phase2/${exp_name}"
-
-                        # ---------------- Command to run ----------------
-                        CMD="python run_sodef.py \
-                            $FIXED_ARGS \
-                            --phase2_weight_diag $r1 \
-                            --phase2_weight_off_diag $r2 \
-                            --phase2_weight_f $r3 \
-                            $decay_arg \
-                            $optim_args \
-                            $no_prev_args \
-                            --exp_name $exp_name \
-                            --output_dir $output_dir" \
-
-                        echo "Running: $exp_name"
-                        echo "$CMD"
-                        eval $CMD
-                    done
                 done
             done
         done
