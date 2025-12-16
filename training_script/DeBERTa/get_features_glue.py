3c8a3f4f59a2d4054b47e51797c7efe04d40857e
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-16 12:30:35 +0330

    +

--- training_script/DeBERTa/get_features_glue.py
+++ training_script/DeBERTa/get_features_glue.py
@@ -451,6 +451,19 @@ def main():
         data_collator = None
 
     
+    
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
     import torch 
     vocab_size = tokenizer.vocab_size    
     batch_size = 4
@@ -482,18 +495,7 @@ def main():
     print(handy_logits)
     print(outputs.logits)
     exit()
-
-    # Initialize our Trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset if training_args.do_train else None,
-        eval_dataset=eval_dataset if training_args.do_eval else None,
-        compute_metrics=compute_metrics,
-        tokenizer=tokenizer,
-        data_collator=data_collator,
-    )
-
+    
     # Training
     if training_args.do_train:
         checkpoint = None
