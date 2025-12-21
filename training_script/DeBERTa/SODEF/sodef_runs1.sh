5ad6d4aafd508f05b0fbd6a4a6a9b22e8f350836
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-22 02:09:58 +0330

    +

--- training_script/DeBERTa/SODEF/sodef_runs1.sh
+++ training_script/DeBERTa/SODEF/sodef_runs1.sh
@@ -224,8 +224,8 @@ for reg_set in "${reg_sets[@]}"; do
                         --exp_name $exp_name \
                         --output_dir $output_dir \
                         --phase3_freeze_ode_block \ 
-                        --cuda_id 1 \
-                        "
+                        --cuda_id 1" \
+                        
 
                     echo "Running: $exp_name"
                     echo "$CMD"
