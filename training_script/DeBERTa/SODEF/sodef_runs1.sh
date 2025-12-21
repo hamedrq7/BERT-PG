b95fc92f28a947ca3c99dc472253283177471572
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-22 02:07:30 +0330

    +

--- training_script/DeBERTa/SODEF/sodef_runs1.sh
+++ training_script/DeBERTa/SODEF/sodef_runs1.sh
@@ -68,22 +68,22 @@
 # Running Phase2: 
 # /mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaFirstSODEF/Phase1-Tuning/eps_-optim_SGD-lr_1e-3-eps_/phase1/phase1_best_acc_ckpt.pth
 
-EXPS_NAME="Phase3-baseA-topolSodef"
-python run_sodef.py  \
-    --skip_phase1 \
-    --phase2_model "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaFirstSODEF-TopolSodef/r1=10.0_r2=1.0_r3=0.1_exp=1.0_expoff_0.1_expf=50.0_timedf_1.0_trans_1.0_transoff_1.0_T=5.0_dafaultAdam=on_noprev=off_topol_ode=on/phase2/phase2_last_ckpt.pth" \
-    --phase3_freeze_ode_block \
-    --phase3_lr_fc 1e-5 \
-    --phase3_eps_fc_block 1e-8 \
-    --phase3_epochs 20 \
-    --exp_name ${EXPS_NAME} \
-    --output_dir ../DeBERTaSodefPhase3Tuning/${EXPS_NAME} \
-    --bert_feature_dim 1024 \
-    --use_topol_ode \
-    --train_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/train_features.npz' \
-    --test_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/test_features.npz' \
-    --seed 100 \
-    --adv_glue_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/advglue_features.npz' \
+# EXPS_NAME="Phase3-baseA-topolSodef"
+# python run_sodef.py  \
+#     --skip_phase1 \
+#     --phase2_model "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaFirstSODEF-TopolSodef/r1=10.0_r2=1.0_r3=0.1_exp=1.0_expoff_0.1_expf=50.0_timedf_1.0_trans_1.0_transoff_1.0_T=5.0_dafaultAdam=on_noprev=off_topol_ode=on/phase2/phase2_last_ckpt.pth" \
+#     --phase3_freeze_ode_block \
+#     --phase3_lr_fc 1e-5 \
+#     --phase3_eps_fc_block 1e-8 \
+#     --phase3_epochs 20 \
+#     --exp_name ${EXPS_NAME} \
+#     --output_dir ../DeBERTaSodefPhase3Tuning/${EXPS_NAME} \
+#     --bert_feature_dim 1024 \
+#     --use_topol_ode \
+#     --train_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/train_features.npz' \
+#     --test_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/test_features.npz' \
+#     --seed 100 \
+#     --adv_glue_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/advglue_features.npz' \
 
 
 #!/usr/bin/env bash
