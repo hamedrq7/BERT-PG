33ca0cb54b2ccc727a5941f8482813ed33da8d78
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-24 01:24:38 +0330

    +

--- training_script/DeBERTa/SODEF/sodef_runs1.sh
+++ training_script/DeBERTa/SODEF/sodef_runs1.sh
@@ -95,6 +95,8 @@ TE_FEATURE_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/tra
 # PHASE1_MODEL="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaFirstSODEF/Phase1-Tuning/eps_-optim_SGD-lr_1e-3-eps_/phase1/phase1_best_acc_ckpt.pth"
 ADV_GULE_DOR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/advglue_features.npz"
 CUDA_ID=1
+project_name="DeBERTa_SODEF_NEW"
+phase1_model_path="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaSODEF-FreezeFC_lossC/FREEZE_FC_baseA_lossC=0.0/phase1/phase1_best_acc_ckpt.pth"
 FIXED_ARGS="--train_feature_set_dir $TR_FEATURE_DIR \
             --test_feature_set_dir $TE_FEATURE_DIR \
             --adv_glue_feature_set_dir $ADV_GULE_DOR \
@@ -105,6 +107,8 @@ FIXED_ARGS="--train_feature_set_dir $TR_FEATURE_DIR \
             --phase1_epoch 10 --phase1_lr 1e-2 --phase1_optim_eps 1e-3 \
             --bert_feature_dim 1024 \
             --phase3_freeze_ode_block \
+            --wandb_project_name $project_name \
+            --phase1_model_path $phase1_model_path \
             --cuda_id $CUDA_ID"
 
 # ----------- EXPERIMENT PARAMETER SETS -----------
@@ -113,14 +117,9 @@ FIXED_ARGS="--train_feature_set_dir $TR_FEATURE_DIR \
 # base A "10.0 1.0 0.1"
 # base B "10.0 10.0 0.2"
 reg_sets=(
-  "1.0 1.0 1.0"
-  "1.0 0.1 0.1"
-  "0.1 1.0 0.1"
-  "0.1 0.1 1.0"
+  "10.0 1.0 0.1"
 )
-#   "0.1 1.0 1.0"
-#   "1.0 0.1 1.0"
-#   "1.0 1.0 0.1"
+
 
 # phase2_exponent, 1.0
 # phase2_exponent_off, 0.1 
@@ -159,8 +158,8 @@ phase2_param_sets=(
 decay_options=("on")      # "on" or "off"
 default_adam=("on")       # "on" or "sgd"
 no_prevs=("off")          # "on" or "off"
-topol_ode="on"
-
+topol_ode="off"
+lossC_set=(0.1) # 1.0 10.0
 # ----------- LOOP OVER ALL EXPERIMENTS -----------
 
 for reg_set in "${reg_sets[@]}"; do
@@ -168,69 +167,71 @@ for reg_set in "${reg_sets[@]}"; do
 
     for phase_set in "${phase2_param_sets[@]}"; do
         read exp exp_off exp_f time_df trans trans_off integ_t <<< "$phase_set"
-        
-        for decay in "${decay_options[@]}"; do
-            for optim in "${default_adam[@]}"; do
-                for no_prev in "${no_prevs[@]}"; do
+        for lossC in "${lossC_set[@]}"; do
+            for decay in "${decay_options[@]}"; do
+                for optim in "${default_adam[@]}"; do
+                    for no_prev in "${no_prevs[@]}"; do
 
-                    # ---------------- Construct optional args ----------------
-                    decay_arg=""
-                    optim_args=""
-                    no_prev_args=""
-                    ode_args=""
+                        # ---------------- Construct optional args ----------------
+                        decay_arg=""
+                        optim_args=""
+                        no_prev_args=""
+                        ode_args=""
 
-                    if [[ "$topol_ode" == "on" ]]; then
-                        ode_args="--use_topol_ode"
-                    fi
-                    
-                    if [[ "$decay" == "on" ]]; then
-                        decay_arg="--decay_lr"
-                    fi
-
-                    if [[ "$optim" == "on" ]]; then
-                        optim_args="--phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
-                    fi
-
-                    if [[ "$optim" == "sgd" ]]; then
-                        optim_args="--phase2_optim SGD --phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
-                    fi
-
-                    if [[ "$no_prev" == "on" ]]; then
-                        no_prev_args="--no_phase2_use_fc_from_phase1 --no_phase3_use_fc_from_phase2"
-                    fi
-
-                    # ---------------- Construct experiment name ----------------
-                    # exp_name="r1=${r1}_r2=${r2}_r3=${r3}_exp=${exp}_expoff_${exp_off}_expf=${exp_f}_timedf_${time_df}_trans_${trans}_transoff_${trans_off}_T=${integ_t}_dafaultAdam=${optim}_noprev=${no_prev}_topol_ode=${topol_ode}"
-                    exp_name="r1=${r1}_r2=${r2}_r3=${r3}"
-
-                    # ---------------- Output directory ----------------
-                    output_dir="../DeBERTaFirstSODEF-r1r2r3/${exp_name}"
-
-                    # ---------------- Command to run ----------------
-                    CMD="python run_sodef.py \
-                        $FIXED_ARGS \
-                        --phase2_weight_diag $r1 \
-                        --phase2_weight_off_diag $r2 \
-                        --phase2_weight_f $r3 \
-                        --phase2_exponent $exp \
-                        --phase2_exponent_off $exp_off \
-                        --phase2_exponent_f $exp_f \
-                        --phase2_time_df $time_df \
-                        --phase2_trans $trans \
-                        --phase2_trans_off_diag $trans_off \
-                        --phase2_integration_time $integ_t \
-                        $decay_arg \
-                        $optim_args \
-                        $no_prev_args \
-                        $ode_args \
-                        --exp_name $exp_name \
-                        --output_dir $output_dir" \
+                        if [[ "$topol_ode" == "on" ]]; then
+                            ode_args="--use_topol_ode"
+                        fi
                         
+                        if [[ "$decay" == "on" ]]; then
+                            decay_arg="--decay_lr"
+                        fi
 
-                    echo "Running: $exp_name"
-                    echo "$CMD"
-                    eval $CMD
+                        if [[ "$optim" == "on" ]]; then
+                            optim_args="--phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
+                        fi
 
+                        if [[ "$optim" == "sgd" ]]; then
+                            optim_args="--phase2_optim SGD --phase2_lr 0.001 --phase2_eps 1e-08 --no_phase2_amsgrad"
+                        fi
+
+                        if [[ "$no_prev" == "on" ]]; then
+                            no_prev_args="--no_phase2_use_fc_from_phase1 --no_phase3_use_fc_from_phase2"
+                        fi
+
+                        # ---------------- Construct experiment name ----------------
+                        # exp_name="r1=${r1}_r2=${r2}_r3=${r3}_exp=${exp}_expoff_${exp_off}_expf=${exp_f}_timedf_${time_df}_trans_${trans}_transoff_${trans_off}_T=${integ_t}_dafaultAdam=${optim}_noprev=${no_prev}_topol_ode=${topol_ode}"
+                        exp_name="FREEZE_FC_baseA_lossC=${lossC}"
+
+                        # ---------------- Output directory ----------------
+                        output_dir="../DeBERTaSODEF-FreezeFC_lossC/${exp_name}"
+
+                        # ---------------- Command to run ----------------
+                        CMD="python run_sodef.py \
+                            $FIXED_ARGS \
+                            --phase2_weight_diag $r1 \
+                            --phase2_weight_off_diag $r2 \
+                            --phase2_weight_f $r3 \
+                            --phase2_exponent $exp \
+                            --phase2_exponent_off $exp_off \
+                            --phase2_exponent_f $exp_f \
+                            --phase2_time_df $time_df \
+                            --phase2_trans $trans \
+                            --phase2_trans_off_diag $trans_off \
+                            --phase2_integration_time $integ_t \
+                            --phase2_ce_weight $lossC \
+                            $decay_arg \
+                            $optim_args \
+                            $no_prev_args \
+                            $ode_args \
+                            --exp_name $exp_name \
+                            --output_dir $output_dir" \
+                            
+
+                        echo "Running: $exp_name"
+                        echo "$CMD"
+                        eval $CMD
+
+                    done
                 done
             done
         done
