18e5def08ad8d612356cb968602a84282c6eee53
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-24 19:55:04 +0330

    +

--- training_script/DeBERTa/SODEF/colab_runs.sh
+++ training_script/DeBERTa/SODEF/colab_runs.sh
@@ -1,16 +1,35 @@
-EXPS_NAME="baseA+expf=75.0"
-base_dir="/content/drive/MyDrive/Colab Notebooks/DeBERTa-sst2"
+# EXPS_NAME="baseA+expf=75.0"
+# base_dir="/content/drive/MyDrive/Colab Notebooks/DeBERTa-sst2"
+# python run_sodef.py  \
+#     --skip_phase1 \
+#     --phase2_model_path "${base_dir}/phase2_last_ckpt.pth" \
+#     --phase3_model_path "${base_dir}/phase3_best_acc_ckpt.pth" \
+#     --exp_name ${EXPS_NAME} \
+#     --output_dir ../${EXPS_NAME} \
+#     --bert_feature_dim 1024 \
+#     --train_feature_set_dir "${base_dir}/train_features.npz" \
+#     --test_feature_set_dir "${base_dir}/test_features.npz" \
+#     --seed 100 \
+#     --adv_glue_feature_set_dir "${base_dir}/advglue_features.npz" \
+#     --no_wandb \
+#     --denoising_analysis \
+#     # --eigval_analysis \
+
+
+EXPS_NAME="Phase3-FREEZE_FC_baseA_topol=on_lossC=1.0"
 python run_sodef.py  \
     --skip_phase1 \
-    --phase2_model_path "${base_dir}/phase2_last_ckpt.pth" \
-    --phase3_model_path "${base_dir}/phase3_best_acc_ckpt.pth" \
+    --phase2_model "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/DeBERTaSODEF-FreezeFC_lossC/FREEZE_FC_baseA_topol=on_lossC=1.0/phase2/phase2_last_ckpt.pth" \
+    --phase3_freeze_ode_block \
+    --phase3_optim 'SGD' \
+    --phase3_lr_fc 1e-5 \
+    --phase3_eps_fc_block 1e-8 \
+    --phase3_epochs 20 \
     --exp_name ${EXPS_NAME} \
-    --output_dir ../${EXPS_NAME} \
+    --output_dir ../DeBERTaSODEF-FreezeFC_lossC/${EXPS_NAME} \
     --bert_feature_dim 1024 \
-    --train_feature_set_dir "${base_dir}/train_features.npz" \
-    --test_feature_set_dir "${base_dir}/test_features.npz" \
+    --use_topol_ode \
+    --train_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/train_features.npz' \
+    --test_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/test_features.npz' \
     --seed 100 \
-    --adv_glue_feature_set_dir "${base_dir}/advglue_features.npz" \
-    --no_wandb \
-    --denoising_analysis \
-    # --eigval_analysis \
+    --adv_glue_feature_set_dir '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/sst2/feats/advglue_features.npz' \
