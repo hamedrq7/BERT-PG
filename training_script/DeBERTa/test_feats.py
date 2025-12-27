c5c33d7517b1222586ac028a02c11b0077b671bd
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-27 20:14:49 +0330

    +

--- training_script/DeBERTa/test_feats.py
+++ training_script/DeBERTa/test_feats.py
@@ -33,7 +33,30 @@ from SODEF.data_utils import get_single_sst2_feature_dataset
 root = '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/mnli/feats'
 
 
+def accu(model, loader, device, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
 
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(loader):            
+            inputs, targets = inputs.to(device), targets.to(device)
+            x = inputs
+            outputs = model(x)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            
+    avg_loss = test_loss/(batch_idx+1)
+    acc = correct/total
+    return {
+        'acc': acc, 
+        'loss': avg_loss,
+    }
 
 adv_m = get_single_sst2_feature_dataset(f'{root}/adv_glue-m_features.npz')
 adv_mm = get_single_sst2_feature_dataset(f'{root}/adv_glue-mm_features.npz')
@@ -43,12 +66,11 @@ te_m = get_single_sst2_feature_dataset(f'{root}/test-m_features.npz')
 device = torch.device(f'cuda:{0}')
 
 from torch.utils.data import DataLoader
-from SODEF.train_utils import test_ce_one_epoch
 def _test(ds, name, model, device): 
     print('Testing ', name)
     dl = DataLoader(ds, batch_size=128)
 
-    te_res = test_ce_one_epoch(-1, model, dl, device, nn.CrossEntropyLoss(), 110, False, '', '')
+    te_res = accu(model, dl, device, nn.CrossEntropyLoss())
     print('Acc, Loss', te_res['acc'], te_res['loss'])
 
 _test(tr, 'train', dummy_model, device)
