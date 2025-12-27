4e6a1e4982ee35774159d1955b6718be945dbdf8
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-27 17:44:14 +0330

    +

--- training_script/DeBERTa/scripts/get_feats.sh
+++ training_script/DeBERTa/scripts/get_feats.sh
@@ -29,52 +29,18 @@
 #   # --eval_accumulation_steps 1 \
 #  #  --max_val_samples 5 \
 
-# TASK_NAME=mnli
-# EXP_INDEX=feats
-# num_epochs=1
-# warmup=10000
-# lr=0e-1
-# num_gpus=1
-# batch_size=4
-# EXP_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/${TASK_NAME}"
-
-# CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=${num_gpus} --master_port=29501 \
-#   get_features_glue_test.py \
-#   --model_name_or_path $EXP_DIR \
-#   --config_name $EXP_DIR \
-#   --tokenizer_name $EXP_DIR \
-#   --task_name $TASK_NAME \
-#   --do_eval \
-#   --max_seq_length 128 \
-#   --num_train_epochs ${num_epochs} \
-#   --warmup_steps ${warmup} \
-#   --learning_rate ${lr} \
-#   --per_device_train_batch_size ${batch_size} \
-#   --per_device_eval_batch_size ${batch_size} \
-#   --output_dir $EXP_DIR/$EXP_INDEX \
-#   --overwrite_output_dir \
-#   --ignore_data_skip \
-#   --logging_steps 10 \
-#   --logging_dir $EXP_DIR/$EXP_INDEX \
-#   --save_total_limit 1 \
-#   --resume_from_checkpoint $EXP_DIR \
-#   --eval_accumulation_steps 128 \
-#   # --max_val_samples 256 \
-
 TASK_NAME=mnli
 EXP_INDEX=feats
 num_epochs=1
 warmup=10000
 lr=0e-1
 num_gpus=1
-batch_size=4
+batch_size=16
 EXP_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/${TASK_NAME}"
 
 CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=${num_gpus} --master_port=29501 \
   get_features_glue_test.py \
   --model_name_or_path $EXP_DIR \
-  --validation_file "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/dev.json" \
-  --is_adv_glue \
   --config_name $EXP_DIR \
   --tokenizer_name $EXP_DIR \
   --task_name $TASK_NAME \
@@ -92,8 +58,42 @@ CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=${num_gpus} --master_port=29501
   --logging_dir $EXP_DIR/$EXP_INDEX \
   --save_total_limit 1 \
   --resume_from_checkpoint $EXP_DIR \
- #  --eval_accumulation_steps 4 \
-  # --max_val_samples 5 \
+  --eval_accumulation_steps 128 \
+  # --max_val_samples 256 \
+
+# TASK_NAME=mnli
+# EXP_INDEX=feats
+# num_epochs=1
+# warmup=10000
+# lr=0e-1
+# num_gpus=1
+# batch_size=4
+# EXP_DIR="/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/${TASK_NAME}"
+
+# CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=${num_gpus} --master_port=29501 \
+#   get_features_glue_test.py \
+#   --model_name_or_path $EXP_DIR \
+#   --validation_file "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/dev.json" \
+#   --is_adv_glue \
+#   --config_name $EXP_DIR \
+#   --tokenizer_name $EXP_DIR \
+#   --task_name $TASK_NAME \
+#   --do_eval \
+#   --max_seq_length 128 \
+#   --num_train_epochs ${num_epochs} \
+#   --warmup_steps ${warmup} \
+#   --learning_rate ${lr} \
+#   --per_device_train_batch_size ${batch_size} \
+#   --per_device_eval_batch_size ${batch_size} \
+#   --output_dir $EXP_DIR/$EXP_INDEX \
+#   --overwrite_output_dir \
+#   --ignore_data_skip \
+#   --logging_steps 10 \
+#   --logging_dir $EXP_DIR/$EXP_INDEX \
+#   --save_total_limit 1 \
+#   --resume_from_checkpoint $EXP_DIR \
+#  #  --eval_accumulation_steps 4 \
+#   # --max_val_samples 5 \
 
 # TASK_NAME=sst2
 # EXP_INDEX=feats
