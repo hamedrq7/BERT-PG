460fea6c2f4be023aa97684dd0f4b7bbf50b728a
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-27 20:10:57 +0330

    +

---
+++ training_script/DeBERTa/test_feats.py
@@ -0,0 +1,71 @@
+import torch
+from safetensors.torch import load_file
+from transformers.models.deberta import DebertaForSequenceClassification
+"""
+For bert-base-cased:
+.... 'bert.encoder.layer.9.intermediate.dense.bias', 'bert.encoder.layer.9.intermediate.dense.weight', 'bert.encoder.layer.9.output.LayerNorm.bias', 'bert.encoder.layer.9.output.LayerNorm.weight', 'bert.encoder.layer.9.output.dense.bias', 'bert.encoder.layer.9.output.dense.weight', 'bert.pooler.dense.bias', 'bert.pooler.dense.weight', 'classifier.bias', 'classifier.weight']
+"""
+
+# ******* the nn.Dropout does not have parameters ********** # Dropout(p=0.1, inplace=False)
+
+weights = load_file("/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/mnli/model.safetensors")
+print(weights.keys())
+
+dims = weights['classifier.weight'].shape
+print(dims)
+print(type(weights['classifier.weight']))
+
+
+import torch.nn as nn 
+class DeBertCLF(nn.Module): 
+    def __init__(self, dim_in, num_classes): 
+        super(DeBertCLF, self).__init__()
+        self.classifier = nn.Linear(dim_in, num_classes)
+
+    def forward(self, x): 
+        return self.classifier(self.dropout(x))
+
+dummy_model = DeBertCLF(dims[1], dims[0])
+missing_keys, unexpected_keys = dummy_model.load_state_dict(weights, strict=False)
+print('missing_keys', missing_keys, 'len unexpected_keys', len(unexpected_keys))
+
+from SODEF.data_utils import get_single_sst2_feature_dataset
+root = '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/DeBERTa/models/DeBERTs/large/mnli/feats'
+
+
+
+
+adv_m = get_single_sst2_feature_dataset(f'{root}/adv_glue-m_features.npz')
+adv_mm = get_single_sst2_feature_dataset(f'{root}/adv_glue-mm_features.npz')
+tr = get_single_sst2_feature_dataset(f'{root}/train_features.npz')
+te_mm = get_single_sst2_feature_dataset(f'{root}/test-mm_features.npz')
+te_m = get_single_sst2_feature_dataset(f'{root}/test-m_features.npz')
+device = torch.device(f'cuda:{0}')
+
+from torch.utils.data import DataLoader
+from SODEF.train_utils import test_ce_one_epoch
+def _test(ds, name, model, device): 
+    print('Testing ', name)
+    dl = DataLoader(ds, batch_size=128)
+
+    te_res = test_ce_one_epoch(-1, model, dl, device, nn.CrossEntropyLoss(), 110, False, '', '')
+    print('Acc, Loss', te_res['acc'], te_res['loss'])
+
+_test(tr, 'train', dummy_model, device)
+_test(te_m, 'test-m', dummy_model, device)
+_test(te_mm, 'test-mm', dummy_model, device)
+_test(adv_m, 'adv-m', dummy_model, device)
+_test(adv_mm, 'adv-mm', dummy_model, device)
+
+# torch.save(dummy_model.state_dict(), "/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/BERT-PG/training_script/BERT/models/no_trainer/sst2/bert_clf.pth")
+
+
+"""
+later do this: 
+state_dict = new_model.state_dict()
+
+state_dict["classifier.weight"] = weights["classifier.weight"]
+state_dict["classifier.bias"] = weights["classifier.bias"]
+
+new_model.load_state_dict(state_dict)
+"""
